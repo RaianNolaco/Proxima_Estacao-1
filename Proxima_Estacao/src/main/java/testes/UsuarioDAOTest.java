74fc2502eb407237da982f5110175fc44f541f71
William Araujo <will.araujo777@gmail.com>
2021-06-24 10:47:31 -0300

    Inserir funcionando na classe teste

--- Proxima_Estacao/src/main/java/testes/UsuarioDAOTest.java
+++ Proxima_Estacao/src/main/java/testes/UsuarioDAOTest.java
@@ -1,7 +1,15 @@
 package testes;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.beans.Status;
 import model.beans.Usuario;
+import model.dao.UsuarioDAO;
+
 import org.junit.jupiter.api.Test;
 
 class UsuarioDAOTest {
@@ -9,7 +17,25 @@ class UsuarioDAOTest {
 	@Test
 	public void InserirUsuario() {
 		
-		Usuario usuario = new Usuario("", null, null, null, null, null, null, null, null);
+		UsuarioDAO dao = new UsuarioDAO();
+		Status status = new Status();
+		status.setId_status(1);
+		Date date = null;
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			date = formatter.parse("17/07/2002");
+		} catch (ParseException e) {
+			System.err.print("Erro ao formatar");
+		}
+		
+		Usuario usuario = new Usuario("William", "Araujo", "Will", "w@gmail.com", status, "zees", "8976543", date, "7654321");
+		if(dao.inserirUsuario(usuario)) {
+			System.out.println("deu certo");
+		}else {
+			fail("Puts");
+		}
+		
 		
 		
 	}
