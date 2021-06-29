44999bb8daacd7f8aae7f0d5e6809d4b8cb0a0d8
William Araujo <will.araujo777@gmail.com>
2021-06-28 23:09:03 -0300

    Adicionando o publicacao DAO e o metodo de insert
    Criei o metodo de inserir publicacao.
    
    Co-Authored-By: Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>

--- Proxima_Estacao/src/main/java/model/dao/UsuarioDAO.java
+++ Proxima_Estacao/src/main/java/model/dao/UsuarioDAO.java
@@ -9,6 +9,8 @@ import connection.ConnectionFactory;
 
 public class UsuarioDAO {
 	private Connection con = null;
+	
+	
 	//metodo responsável 
 	public boolean inserirUsuario(Usuario usuario) {
 		String sql = "INSERT INTO usuario (nome, sobrenome, apelido, email, senha, numero, id_status, data_nascimento, cpf)"
@@ -19,24 +21,23 @@ public class UsuarioDAO {
 		
 		try {
 			stmt = con.prepareStatement(sql);
-			stmt.setInt   (1,  usuario.getIdUsuario());
-			stmt.setString(2,  usuario.getNomeUsuario());
-			stmt.setString(3,  usuario.getSobrenomeUsuario());
-			stmt.setString(4,  usuario.getApelido());
-			stmt.setString(5,  usuario.getEmail());
-			stmt.setString(6,  usuario.getSenha());
-			stmt.setString(7,  usuario.getNumero());
-			stmt.setInt   (8,  usuario.getIdStatus().getId_status());
-			stmt.setDate  (9,  new java.sql.Date(usuario.getData_nascimento().getTime()));
-			stmt.setString(10, usuario.getCpf());
-			stmt.executeUpdate();
-			con.close();
-			stmt.close();
+			stmt.setString(1,  usuario.getNomeUsuario());
+			stmt.setString(2,  usuario.getSobrenomeUsuario());
+			stmt.setString(3,  usuario.getApelido());
+			stmt.setString(4,  usuario.getEmail());
+			stmt.setString(5,  usuario.getSenha());
+			stmt.setString(6,  usuario.getNumero());
+			stmt.setInt   (7,  usuario.getIdStatus().getId_status());
+			stmt.setDate  (8,  new java.sql.Date(usuario.getData_nascimento().getTime()));
+			stmt.setString(9, usuario.getCpf());
+			stmt.execute();
             return true;
 			
 		} catch (SQLException e) {
 			System.err.println("Erro: " +e);
 			return false;
+	    } finally {
+	    	
 	    }
 }
 	
