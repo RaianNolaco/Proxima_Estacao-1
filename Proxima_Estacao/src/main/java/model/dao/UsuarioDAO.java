b00acd857257cb1922c903e6e2903380dd51f804
William Araujo <will.araujo777@gmail.com>
2021-06-29 15:38:40 -0300

    Update UsuarioDAO.java

--- Proxima_Estacao/src/main/java/model/dao/UsuarioDAO.java
+++ Proxima_Estacao/src/main/java/model/dao/UsuarioDAO.java
@@ -9,36 +9,35 @@ import connection.ConnectionFactory;
 
 public class UsuarioDAO {
 	private Connection con = null;
-	
-	
-	//metodo responsável 
+
+	// metodo responsável
 	public boolean inserirUsuario(Usuario usuario) {
 		String sql = "INSERT INTO usuario (nome, sobrenome, apelido, email, senha, numero, id_status, data_nascimento, cpf)"
 				+ "VALUES (?,?,?,?,?,?,?,?,?,?)";
-		
+
 		con = ConnectionFactory.getConnection();
 		PreparedStatement stmt = null;
-		
+
 		try {
 			stmt = con.prepareStatement(sql);
-			stmt.setString(1,  usuario.getNomeUsuario());
-			stmt.setString(2,  usuario.getSobrenomeUsuario());
-			stmt.setString(3,  usuario.getApelido());
-			stmt.setString(4,  usuario.getEmail());
-			stmt.setString(5,  usuario.getSenha());
-			stmt.setString(6,  usuario.getNumero());
-			stmt.setInt   (7,  usuario.getIdStatus().getId_status());
-			stmt.setDate  (8,  new java.sql.Date(usuario.getData_nascimento().getTime()));
+			stmt.setString(1, usuario.getNomeUsuario());
+			stmt.setString(2, usuario.getSobrenomeUsuario());
+			stmt.setString(3, usuario.getApelido());
+			stmt.setString(4, usuario.getEmail());
+			stmt.setString(5, usuario.getSenha());
+			stmt.setString(6, usuario.getNumero());
+			stmt.setInt(7, usuario.getIdStatus().getId_status());
+			stmt.setDate(8, new java.sql.Date(usuario.getData_nascimento().getTime()));
 			stmt.setString(9, usuario.getCpf());
 			stmt.execute();
-            return true;
-			
+			return true;
+
 		} catch (SQLException e) {
-			System.err.println("Erro: " +e);
+			System.err.println("Erro: " + e);
 			return false;
-	    } finally {
-	    	ConnectionFactory.closeConnection(con, stmt);
-	    }
-}
-	
+		} finally {
+			ConnectionFactory.closeConnection(con, stmt);
+		}
+	}
+
 }
