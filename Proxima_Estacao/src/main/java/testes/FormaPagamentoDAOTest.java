a8724559d2a7dd103d52b39276075532f8af48c2
William Araujo <will.araujo777@gmail.com>
2021-06-29 23:54:03 -0300

    Add metodo listarTIpos
    Este metodo é reponsável por listar todos os tipos_categoria do banco de dados.

--- Proxima_Estacao/src/main/java/testes/FormaPagamentoDAOTest.java
+++ Proxima_Estacao/src/main/java/testes/FormaPagamentoDAOTest.java
@@ -6,17 +6,14 @@ import model.dao.FormaPagamentoDAO;
 
 class FormaPagamentoDAOTest {
 	FormaPagamentoDAO dao = new FormaPagamentoDAO();
-	
+
 	@Test
 	public void listarPublicacoes() {
-		
+
 		for (Forma_pagamento forma : dao.listarFormas()) {
-			System.out.println("\nId: " + forma.getId_forma_pag()
-			+ "\nNome: " + forma.getNome());
-		
-		
+			System.out.println("\nId: " + forma.getId_forma_pag() + "\nNome: " + forma.getNome());
+		}
+
 	}
 
-}
-	
 }
